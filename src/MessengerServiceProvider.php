f9ca1dec893cde5437e4f4818cce5fa9639dc7fc
Mojtaba_Malekfar <mojtaba_malekfar@ymail.com>
2019-10-18 14:22:20 +0330

    change address of config file

--- src/MessengerServiceProvider.php
+++ src/MessengerServiceProvider.php
@@ -43,7 +43,7 @@ class MessengerServiceProvider extends ServiceProvider
     protected function configure()
     {
         $this->mergeConfigFrom(
-            base_path('vendor/cmgmyr/messenger/config/config.php'),
+            base_path('vendor/malekfar/cmgmyr-messenger/config/config.php'),
             'messenger'
         );
     }
@@ -57,11 +57,11 @@ class MessengerServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                base_path('vendor/cmgmyr/messenger/config/config.php') => config_path('messenger.php'),
+                base_path('vendor/malekfar/cmgmyr-messenger/config/config.php') => config_path('messenger.php'),
             ], 'config');
 
             $this->publishes([
-                base_path('vendor/cmgmyr/messenger/migrations') => base_path('database/migrations'),
+                base_path('vendor/malekfar/cmgmyr-messenger/migrations') => base_path('database/migrations'),
             ], 'migrations');
         }
     }
